5b413fd1fca642c05886453c64a86db57f6b8112
Daniel Fried <dfried@email.arizona.edu>
2012-11-01 22:30:54 -0700

    database ordering modified

--- mtbolt/settings.py
+++ mtbolt/settings.py
@@ -8,18 +8,17 @@ def abspath(*args):
     return os.path.abspath(os.path.join(currpath, '..', *args))
 
 PRODUCTION=os.environ.get('PRODUCTION', None)
+DATABASES = {}
     
 if not PRODUCTION:
     DEBUG = True
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': abspath('mtbolt.db'),                      # Or path to database file if using sqlite3.
-            'USER': '',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': abspath('mtbolt.db'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 else: # production
     DEBUG = False
@@ -45,12 +44,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    # this will be overridden depending on value of PRODUCTION
-    'default': { 
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
