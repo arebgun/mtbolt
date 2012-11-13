f1c94982970cb2617403c20d72e5c5ddb4a46780
Daniel Fried <dfried@email.arizona.edu>
2012-11-13 13:36:34 -0700

    approve and reject in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from tasks.models import DescriptionTask, DescriptionQuestion
 
+def approve(modeladmin, request, queryset):
+    queryset.update(approved=True)
+approve.short_description = 'Approve selected decription tasks'
+
+def reject(modeladmin, request, queryset):
+    queryset.update(approved=False)
+reject.short_description = 'Reject selected decription tasks'
+
 class DescriptionTaskAdmin(admin.ModelAdmin):
     fields = ('completion_code', 'approved')
     readonly_fields = ('completion_code',)
     search_fields = ('completion_code',)
     list_display = ('completion_code', 'approved')
     list_filter = ('approved', 'created', 'modified')
+    actions = [approve, reject]
 
 class DescriptionQuestionAdmin(admin.ModelAdmin):
     list_display = ('scene', 'entity', 'object_description', 'location_description', 'answer',)
