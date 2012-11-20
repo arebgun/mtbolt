4e8b4641497fb49e0bb0d89dc9b38e879116c8ae
Daniel Fried <dfried@email.arizona.edu>
2012-11-20 01:33:39 -0700

    added js validation

--- static/js/object_description.js
+++ static/js/object_description.js
@@ -3,17 +3,11 @@ $(document).ready(function () {
     $('form').submit(function () {
         $('.error').remove();
 
-        /* insert validation here */
-        /*
-        $('.object_description, .location_description').each(function () {
-            if (/^\s*$/.test(this.value)) {
-                $(this).after('<div class="error">This field is required.</div>');
-            }
-            if (/\d/.test(this.value)) {
-                $(this).after('<div class="error">No digits allowed. Do not use object numbers to refer to objects.</div>');
+        $('.object_binding').each(function () {
+            if (isNaN(this.value)) {
+                $(this).after('<div class="error">Please select an object</div>');
             }
         });
-        */
 
         if ($('.error').length > 0) {
             alert('Please answer all questions correctly.');
