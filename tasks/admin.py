a63beab7d86b23784ddca76dd4aa4bd7d0eaf820
Daniel Fried <dfried@email.arizona.edu>
2012-12-13 11:51:53 -0700

    fixed csv export

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.conf.urls import patterns
 from tasks.models import DescriptionTask, DescriptionQuestion
-import csv
-from django.http import HttpResponse
+from mtbolt.actions import export_as_csv_action
 
 def approve(modeladmin, request, queryset):
     queryset.update(approved=True)
@@ -32,25 +30,11 @@ class DescriptionQuestionAdmin(admin.ModelAdmin):
     list_display = ('scene', 'entity', 'object_description', 'location_description', 'answer', 'use_in_object_tasks',)
     list_filter = ('created', 'modified', 'scene', 'task')
 
-    actions = [use, do_not_use]
+    actions = [use, do_not_use, export_as_csv_action('Export to CSV', fields=list_display)]
 
-    def get_urls(self):
-        urls = super(DescriptionQuestionAdmin, self).get_urls()
-        my_urls = patterns('',
-                           (r'^responses/$', self.responses_view)
-                          )
-        return my_urls + urls
-
-    def responses_view(self, request):
-        response = HttpResponse(mimetype='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="responses.csv"'
-
-        writer = csv.writer(response)
-        writer.writerow(['scene', 'entity', 'object_description', 'location_description', 'old_description', 'completion_code', 'approved'])
-        for dq in DescriptionQuestion.objects.all():
-            writer.writerow([dq.scene.name, dq.entity.name, dq.object_description, dq.location_description, dq.answer, dq.task.completion_code, dq.task.approved])
-
-        return response
+    def task_approved(self, obj):
+        return '%s'%(obj.task.approved)
+    task_approved.short_description = 'Approved'
 
 admin.site.register(DescriptionTask, DescriptionTaskAdmin)
 admin.site.register(DescriptionQuestion, DescriptionQuestionAdmin)
