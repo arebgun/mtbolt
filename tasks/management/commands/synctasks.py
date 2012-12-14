2bc6f6ca25b56af953ad760d1130cb176dbdc5be
Daniel Fried <dfried@email.arizona.edu>
2012-12-13 19:58:53 -0700

    fixed oo bug in synctasks

--- tasks/management/commands/synctasks.py
+++ tasks/management/commands/synctasks.py
@@ -2,28 +2,29 @@ from django.core.management.base import BaseCommand
 from tasks.models import DescriptionTask
 from mtbolt.mturk import open_connection, get_open_assignments, code_for_assignment
 
+def sync_turk_tasks():
+    connection = open_connection()
+    assignments = get_open_assignments(connection)
+    for a in assignments:
+        code = code_for_assignment(a)
+        matching_tasks = DescriptionTask.objects.filter(completion_code=code)
+        if matching_tasks:
+            if len(matching_tasks) > 1:
+                print 'multiple tasks found for %s: %s' % (code, len(matching_tasks))
+            description_task = matching_tasks[0]
+            if description_task.approved == True:
+                print 'approving %s, %s' % (a, code)
+            elif description_task.approved == False:
+                print 'would reject %s, %s' % (a, code)
+            else:
+                print 'val for %s, %s = %s' % (a, code, description_task.approved)
+                # connection.approve_assignment(a)
+        else:
+            print 'no tasks found for %s' % code
+
 class Command(BaseCommand):
     help = "Approves or rejects active Turk assignments that have been flagged in the database"
 
-    def sync_turk_tasks():
-        connection = open_connection()
-        assignments = get_open_assignments(connection)
-        for a in assignments:
-            code = code_for_assignment(a)
-            matching_tasks = DescriptionTask.objects.filter(completion_code=code)
-            if matching_tasks:
-                if len(matching_tasks) > 1:
-                    print 'multiple tasks found for %s: %s' % (code, len(matching_tasks))
-                description_task = matching_tasks[0]
-                if description_task.approved == True:
-                    print 'approving %s, %s' % (a, code)
-                elif description_task.approved == False:
-                    print 'would reject %s, %s' % (a, code)
-                else:
-                    print 'val for %s, %s = %s' % (a, code, description_task.approved)
-                    # connection.approve_assignment(a)
-            else:
-                print 'no tasks found for %s' % code
 
     def handle(self, *args, **options):
-        self.sync_turk_tasks()
+        sync_turk_tasks()
