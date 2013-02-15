5a1b1ca3ac946d6861ce1a2b74dc98d0cc3ad32b
Daniel Fried <dfried@email.arizona.edu>
2013-02-15 10:21:07 -0700

    better acc calc

--- object_tasks/management/commands/object_task_stats.py
+++ object_tasks/management/commands/object_task_stats.py
@@ -4,8 +4,11 @@ from tasks.models import DescriptionQuestion
 from mtbolt.settings import PROJECT_ROOT
 from os.path import join
 
+def is_correct(entity_binding):
+    return entity_binding.binding == int(entity_binding.description.entity.name)
+
 def num_correct(task):
-        return sum(map(lambda eb: eb.binding == int(eb.description.entity.name), task.entity_bindings.all()))
+        return sum(map(is_correct, task.entity_bindings.all()))
 
 def descriptions_passing_extra_validation():
     with open(join(PROJECT_ROOT, 'passed_in_all.txt')) as f:
@@ -18,6 +21,7 @@ def print_stats(extra_validation=False):
     if extra_validation:
         sentences = [s for s in sentences if s.id in validated_set]
 
+    '''
     obj_desc_tasks = object_tasks.models.ObjectDescriptionTask.objects.all()
 
     print 'number of HITs completed: %d' % len(obj_desc_tasks)
@@ -26,6 +30,13 @@ def print_stats(extra_validation=False):
     print correct_per[:30]
     print sum(correct_per)
     print 'average bindings correct per HIT: %f' % (float(sum(correct_per)) / (5 * len(correct_per)))
+    '''
+
+    entity_bindings = [eb for eb in object_tasks.models.EntityBinding.objects.all()
+                       if eb.description in sentences]
+
+    print 'number of bindings: %d' % len(entity_bindings)
+    print 'Turker accuracy: %f' % (float(sum(map(is_correct, entity_bindings))) / len(entity_bindings))
 
     bindings_per_sent = map(lambda sent: sent.entity_bindings.count(), sentences)
     num_proofed = len([n for n in bindings_per_sent if n > 0])
