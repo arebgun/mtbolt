61351b28a07bb5088406c10e357bfd3f5712f8aa
Antons Rebguns <arebgun@gmail.com>
2013-11-19 10:46:38 -0700

    fix db constraints to point to correct table/pk combo, sqlite was working, pg broke

--- mtbolt/settings.py
+++ mtbolt/settings.py
@@ -14,11 +14,23 @@ PROJECT_ROOT = abspath('.')
 
 if not PRODUCTION:
     DEBUG = True
+    # DATABASES['default'] = {
+    #     'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #     'NAME': abspath('mtbolt.db'),                      # Or path to database file if using sqlite3.
+    #     'USER': '',                      # Not used with sqlite3.
+    #     'PASSWORD': '',                  # Not used with sqlite3.
+    #     'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    #     'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    # }
+
+    PG_USER = os.environ.get('PG_USER')
+    PG_PASSWORD = os.environ.get('PG_PASSWORD')
+
     DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': abspath('mtbolt.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', #'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mylocaldb3',                      # Or path to database file if using sqlite3.
+        'USER': PG_USER,                      # Not used with sqlite3.
+        'PASSWORD': PG_PASSWORD,                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
